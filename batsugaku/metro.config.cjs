f0831ca7758acd78236519bcb55c85c883ee64d6
栗田孟 <akito14140099@gmail.com>
2025-11-22 18:55:58 +0900

    動作確認環境完成

---
+++ batsugaku/metro.config.cjs
@@ -0,0 +1,11 @@
+/**
+ * Metro 設定ファイル
+ *
+ * 今回は React Native CLI が要求する「設定ファイルの存在」を満たすための最小構成です。
+ * 特別なカスタマイズをしていないため、空オブジェクトをそのままエクスポートします。
+ */
+
+/** @type {import('metro-config').ConfigT} */
+module.exports = {};
+
+
